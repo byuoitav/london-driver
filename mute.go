a41a4eb75b9b2a6e4e5e98e94cd81a4bc28298cb
Andrew Werner <klinc9@gmail.com>
2020-06-23 10:13:45 -0600

    updating driver to match new api functions

--- mute.go
+++ mute.go
@@ -11,83 +11,89 @@ import (
 )
 
 // GetMutedByBlock returns true if the given block is muted.
-func (d *DSP) GetMutedByBlock(ctx context.Context, block string) (bool, error) {
-	subscribe, err := buildSubscribeCommand(methodSubscribe, stateMute, block, minSubscribeInterval)
-	if err != nil {
-		return false, fmt.Errorf("unable to build subscribe command: %w", err)
-	}
+func (d *DSP) GetMutes(ctx context.Context, blocks []string) (map[string]bool, error) {
+	toReturn := make(map[string]bool)
 
-	unsubscribe, err := buildUnsubscribeCommand(methodUnsubscribe, stateMute, block)
-	if err != nil {
-		return false, fmt.Errorf("unable to build unsubscribe command: %w", err)
-	}
-
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-	defer cancel()
-
-	var resp []byte
-
-	err = d.pool.Do(ctx, func(conn connpool.Conn) error {
-		d.infof("Getting mute on %v", block)
-		d.debugf("Writing subscribe command: 0x%x", subscribe)
-
-		conn.SetWriteDeadline(time.Now().Add(3 * time.Second))
-
-		n, err := conn.Write(subscribe)
-		switch {
-		case err != nil:
-			return fmt.Errorf("unable to write subscribe command: %w", err)
-		case n != len(subscribe):
-			return fmt.Errorf("unable to write subscribe command: wrote %v/%v bytes", n, len(subscribe))
-		}
-
-		resp, err = conn.ReadUntil(asciiETX, 3*time.Second)
+	for _, block := range blocks {
+		subscribe, err := buildSubscribeCommand(methodSubscribe, stateMute, block, minSubscribeInterval)
 		if err != nil {
-			return fmt.Errorf("unable to read response: %w", err)
+			return toReturn, fmt.Errorf("unable to build subscribe command: %w", err)
 		}
 
-		d.debugf("Got response: 0x%x", resp)
-		d.debugf("Writing unsubscribe command: 0x%x", unsubscribe)
+		unsubscribe, err := buildUnsubscribeCommand(methodUnsubscribe, stateMute, block)
+		if err != nil {
+			return toReturn, fmt.Errorf("unable to build unsubscribe command: %w", err)
+		}
 
-		conn.SetWriteDeadline(time.Now().Add(3 * time.Second))
+		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
 
-		n, err = conn.Write(unsubscribe)
+		var resp []byte
+
+		err = d.pool.Do(ctx, func(conn connpool.Conn) error {
+			d.infof("Getting mute on %v", block)
+			d.debugf("Writing subscribe command: 0x%x", subscribe)
+
+			conn.SetWriteDeadline(time.Now().Add(3 * time.Second))
+
+			n, err := conn.Write(subscribe)
+			switch {
+			case err != nil:
+				return fmt.Errorf("unable to write subscribe command: %w", err)
+			case n != len(subscribe):
+				return fmt.Errorf("unable to write subscribe command: wrote %v/%v bytes", n, len(subscribe))
+			}
+
+			resp, err = conn.ReadUntil(asciiETX, 3*time.Second)
+			if err != nil {
+				return fmt.Errorf("unable to read response: %w", err)
+			}
+
+			d.debugf("Got response: 0x%x", resp)
+			d.debugf("Writing unsubscribe command: 0x%x", unsubscribe)
+
+			conn.SetWriteDeadline(time.Now().Add(3 * time.Second))
+
+			n, err = conn.Write(unsubscribe)
+			switch {
+			case err != nil:
+				return fmt.Errorf("unable to write unsubscribe command: %w", err)
+			case n != len(unsubscribe):
+				return fmt.Errorf("unable to write unsubscribe command: wrote %v/%v bytes", n, len(subscribe))
+			}
+
+			return nil
+		})
+		if err != nil {
+			return toReturn, err
+		}
+
+		resp, err = decode(resp)
+		if err != nil {
+			return toReturn, fmt.Errorf("unable to decode response: %w", err)
+		}
+
+		data := resp[len(resp)-1:]
+
 		switch {
-		case err != nil:
-			return fmt.Errorf("unable to write unsubscribe command: %w", err)
-		case n != len(unsubscribe):
-			return fmt.Errorf("unable to write unsubscribe command: wrote %v/%v bytes", n, len(subscribe))
+		case bytes.Equal(data, []byte{0}):
+			d.infof("Mute on %v is %v", block, false)
+			toReturn[block] = false
+		case bytes.Equal(data, []byte{1}):
+			d.infof("Mute on %v is %v", block, true)
+			toReturn[block] = true
+		default:
+			return toReturn, errors.New("bad data in response from DSP")
 		}
-
-		return nil
-	})
-	if err != nil {
-		return false, err
 	}
 
-	resp, err = decode(resp)
-	if err != nil {
-		return false, fmt.Errorf("unable to decode response: %w", err)
-	}
-
-	data := resp[len(resp)-1:]
-
-	switch {
-	case bytes.Equal(data, []byte{0}):
-		d.infof("Mute on %v is %v", block, false)
-		return false, nil
-	case bytes.Equal(data, []byte{1}):
-		d.infof("Mute on %v is %v", block, true)
-		return true, nil
-	default:
-		return false, errors.New("bad data in response from DSP")
-	}
+	return toReturn, nil
 }
 
 // SetMutedByBlock sets the mute state on the given block.
-func (d *DSP) SetMutedByBlock(ctx context.Context, block string, muted bool) error {
+func (d *DSP) SetMute(ctx context.Context, block string, mute bool) error {
 	data := []byte{0x00, 0x00, 0x00, 0x00}
-	if muted {
+	if mute {
 		data[3] = 0x01
 	}
 
@@ -100,7 +106,7 @@ func (d *DSP) SetMutedByBlock(ctx context.Context, block string, muted bool) err
 	defer cancel()
 
 	err = d.pool.Do(ctx, func(conn connpool.Conn) error {
-		d.infof("Setting mute on %v to %v", block, muted)
+		d.infof("Setting mute on %v to %v", block, mute)
 		d.debugf("Writing command: 0x%x", cmd)
 
 		conn.SetWriteDeadline(time.Now().Add(3 * time.Second))
--- volume.go
+++ volume.go
@@ -14,78 +14,84 @@ const (
 )
 
 // GetVolumeByBlock returns the volume [0, 100] of the given block.
-func (d *DSP) GetVolumeByBlock(ctx context.Context, block string) (int, error) {
-	subscribe, err := buildSubscribeCommand(methodSubscribePercent, stateGain, block, minSubscribeInterval)
-	if err != nil {
-		return 0, fmt.Errorf("unable to build subscribe command: %w", err)
-	}
+func (d *DSP) GetVolumes(ctx context.Context, blocks []string) (map[string]int, error) {
+	toReturn := make(map[string]int)
 
-	unsubscribe, err := buildUnsubscribeCommand(methodUnsubscribePercent, stateGain, block)
-	if err != nil {
-		return 0, fmt.Errorf("unable to build unsubscribe command: %w", err)
-	}
-
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-	defer cancel()
-
-	var resp []byte
-
-	err = d.pool.Do(ctx, func(conn connpool.Conn) error {
-		d.infof("Getting volume on %v", block)
-		d.debugf("Writing subscribe command: 0x%x", subscribe)
-
-		conn.SetWriteDeadline(time.Now().Add(3 * time.Second))
-
-		n, err := conn.Write(subscribe)
-		switch {
-		case err != nil:
-			return fmt.Errorf("unable to write subscribe command: %w", err)
-		case n != len(subscribe):
-			return fmt.Errorf("unable to write subscribe command: wrote %v/%v bytes", n, len(subscribe))
-		}
-
-		resp, err = conn.ReadUntil(asciiETX, 3*time.Second)
+	for _, block := range blocks {
+		subscribe, err := buildSubscribeCommand(methodSubscribePercent, stateGain, block, minSubscribeInterval)
 		if err != nil {
-			return fmt.Errorf("unable to read response: %w", err)
+			return toReturn, fmt.Errorf("unable to build subscribe command: %w", err)
 		}
 
-		d.debugf("Got response: 0x%x", resp)
-		d.debugf("Writing unsubscribe command: 0x%x", unsubscribe)
-
-		conn.SetWriteDeadline(time.Now().Add(3 * time.Second))
-
-		n, err = conn.Write(unsubscribe)
-		switch {
-		case err != nil:
-			return fmt.Errorf("unable to write unsubscribe command: %w", err)
-		case n != len(unsubscribe):
-			return fmt.Errorf("unable to write unsubscribe command: wrote %v/%v bytes", n, len(subscribe))
+		unsubscribe, err := buildUnsubscribeCommand(methodUnsubscribePercent, stateGain, block)
+		if err != nil {
+			return toReturn, fmt.Errorf("unable to build unsubscribe command: %w", err)
 		}
 
-		return nil
-	})
-	if err != nil {
-		return 0, err
+		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+
+		var resp []byte
+
+		err = d.pool.Do(ctx, func(conn connpool.Conn) error {
+			d.infof("Getting volume on %v", block)
+			d.debugf("Writing subscribe command: 0x%x", subscribe)
+
+			conn.SetWriteDeadline(time.Now().Add(3 * time.Second))
+
+			n, err := conn.Write(subscribe)
+			switch {
+			case err != nil:
+				return fmt.Errorf("unable to write subscribe command: %w", err)
+			case n != len(subscribe):
+				return fmt.Errorf("unable to write subscribe command: wrote %v/%v bytes", n, len(subscribe))
+			}
+
+			resp, err = conn.ReadUntil(asciiETX, 3*time.Second)
+			if err != nil {
+				return fmt.Errorf("unable to read response: %w", err)
+			}
+
+			d.debugf("Got response: 0x%x", resp)
+			d.debugf("Writing unsubscribe command: 0x%x", unsubscribe)
+
+			conn.SetWriteDeadline(time.Now().Add(3 * time.Second))
+
+			n, err = conn.Write(unsubscribe)
+			switch {
+			case err != nil:
+				return fmt.Errorf("unable to write unsubscribe command: %w", err)
+			case n != len(unsubscribe):
+				return fmt.Errorf("unable to write unsubscribe command: wrote %v/%v bytes", n, len(subscribe))
+			}
+
+			return nil
+		})
+		if err != nil {
+			return toReturn, err
+		}
+
+		resp, err = decode(resp)
+		if err != nil {
+			return toReturn, fmt.Errorf("unable to decode response: %w", err)
+		}
+
+		data := resp[len(resp)-4:]
+		vol := binary.BigEndian.Uint32(data)
+
+		vol = vol / volumeScaleFactor
+		vol++
+
+		d.infof("Volume on %v is %v", block, int(vol))
+
+		toReturn[block] = int(vol)
 	}
 
-	resp, err = decode(resp)
-	if err != nil {
-		return 0, fmt.Errorf("unable to decode response: %w", err)
-	}
-
-	data := resp[len(resp)-4:]
-	vol := binary.BigEndian.Uint32(data)
-
-	vol = vol / volumeScaleFactor
-	vol++
-
-	d.infof("Volume on %v is %v", block, int(vol))
-
-	return int(vol), nil
+	return toReturn, nil
 }
 
 // SetVolumeByBlock sets the volume on the given block. Volume must be in the range [0, 100].
-func (d *DSP) SetVolumeByBlock(ctx context.Context, block string, volume int) error {
+func (d *DSP) SetVolume(ctx context.Context, block string, volume int) error {
 	if volume < 0 || volume > 100 {
 		return fmt.Errorf("volume must be in range [0, 100]")
 	}
